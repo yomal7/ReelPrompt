313be89ff24d9d9933b81dc0f2ec15d42fb76077
yomal7 <yomaloshada45@gmail.com>
2025-08-21 18:25:55 +0530

    remove previous backend

--- Backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signup, signin } = require("../controllers/authController");
-const { validateSignup, validateSignin } = require("../middleware/validation");
-const router = express.Router();
-
-router.post("/signup", validateSignup, signup);
-router.post("/signin", validateSignin, signin);
-
-module.exports = router;
